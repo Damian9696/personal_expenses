f43009791fc54108b34738e1f2253af1edab07ff
Damian9696 <damian.ogorek.1996@gmail.com>
2021-06-02 21:47:30 +0200

    Added scroll to widgets;

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,46 +3,50 @@ import 'package:intl/intl.dart';
 import 'package:personal_expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-
   final List<Transaction> userTransaction;
 
   TransactionList(this.userTransaction);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: userTransaction.map((transaction) {
-      return Card(
-          child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-            decoration:
-                BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "\$${transaction.amount}",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blueAccent),
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+            children: userTransaction.map((transaction) {
+          return Card(
+              child: Row(
             children: [
-              Text(
-                transaction.title,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "\$${transaction.amount}",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.blueAccent),
+                ),
               ),
-              Text(
-                DateFormat('dd-MM-yyyy').format(transaction.date),
-                style: TextStyle(color: Colors.grey),
-              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    transaction.title,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    DateFormat('dd-MM-yyyy').format(transaction.date),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      ));
-    }).toList());
+          ));
+        }).toList()),
+      ),
+    );
   }
 }
