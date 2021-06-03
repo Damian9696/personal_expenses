fc761e78d494358274867c4e699d20cc7fd06219
Damian9696 <damian.ogorek.1996@gmail.com>
2021-06-03 13:33:09 +0200

    Replaced SingleChildScrollView with Column into ListView;

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,17 +3,16 @@ import 'package:intl/intl.dart';
 import 'package:personal_expenses/models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  final List<Transaction> userTransaction;
+  final List<Transaction> transactions;
 
-  TransactionList(this.userTransaction);
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-            children: userTransaction.map((transaction) {
+      child: ListView.builder(
+        itemBuilder: (context, index) {
           return Card(
               child: Row(
             children: [
@@ -23,7 +22,7 @@ class TransactionList extends StatelessWidget {
                     BoxDecoration(border: Border.all(color: Colors.blueAccent)),
                 padding: EdgeInsets.all(10),
                 child: Text(
-                  "\$${transaction.amount}",
+                  "\$${transactions[index].amount}",
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
@@ -34,18 +33,19 @@ class TransactionList extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    transaction.title,
+                    transactions[index].title,
                     style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                   Text(
-                    DateFormat('dd-MM-yyyy').format(transaction.date),
+                    DateFormat('dd-MM-yyyy').format(transactions[index].date),
                     style: TextStyle(color: Colors.grey),
                   ),
                 ],
               )
             ],
           ));
-        }).toList()),
+        },
+        itemCount: transactions.length,
       ),
     );
   }
