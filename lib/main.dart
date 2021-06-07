80816c1ccf9be61cf7e929f0b9f39304af4d85eb
Damian9696 <damian.ogorek.1996@gmail.com>
2021-06-07 21:16:56 +0200

    Created getter which filter list of transaction to last seven days; Implemented Chart widget;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:personal_expenses/models/transaction.dart';
+import 'package:personal_expenses/widgets/chart.dart';
 import 'package:personal_expenses/widgets/new_transaction.dart';
 import 'package:personal_expenses/widgets/transaction_list.dart';
 
@@ -43,6 +44,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // Transaction(id: "1", title: "Car", amount: 150.00, date: DateTime.now())
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _transactions.where((transaction) {
+      return transaction.date
+          .isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
   void _addNewTransaction(String newTitle, double newAmount) {
     final newTransaction = Transaction(
         id: DateTime.now().toString(),
@@ -82,14 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text("Chart"),
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_transactions),
           ],
         ),
