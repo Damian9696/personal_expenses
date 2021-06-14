6efeedd3af09ceeab90ef99987ac10c4f85b9b49
Damian9696 <damian.ogorek.1996@gmail.com>
2021-06-14 22:47:47 +0200

    Created logic which conditionally show widgets based on device width;

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -51,11 +51,21 @@ class TransactionList extends StatelessWidget {
                   ),
                   subtitle: Text(DateFormat("dd-MM-yyyy")
                       .format(transactions[index].date)),
-                  trailing: IconButton(
-                    color: Theme.of(context).errorColor,
-                    icon: Icon(Icons.delete),
-                    onPressed: () => deleteTransaction(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 360
+                      ? TextButton.icon(
+                          onPressed: () =>
+                              deleteTransaction(transactions[index].id),
+                          icon: Icon(Icons.delete),
+                          label: Text('Delete'),
+                          style: TextButton.styleFrom(
+                              primary: Theme.of(context).errorColor),
+                        )
+                      : IconButton(
+                          color: Theme.of(context).errorColor,
+                          icon: Icon(Icons.delete),
+                          onPressed: () =>
+                              deleteTransaction(transactions[index].id),
+                        ),
                 ),
               );
             },
